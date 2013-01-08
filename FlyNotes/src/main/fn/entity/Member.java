d8135f60f2f03a2754a75de920b125786ccc571d
Saurabh <ice1mind@gmail.com>
2013-01-08 11:34:39 +0530

    partially working register page, name, mail, pass

--- FlyNotes/src/main/fn/entity/Member.java
+++ FlyNotes/src/main/fn/entity/Member.java
@@ -18,6 +18,8 @@ public class Member implements java.io.Serializable {
 	private int id;
 
 	private String name;
+	private String password;
+	private String email;
 
 	@Id
 	@Column(name = "id", unique = true, nullable = false)
@@ -47,10 +49,31 @@ public class Member implements java.io.Serializable {
 	public Member(int id) {
 		this.id = id;
 	}
-
-	public Member(int id, String name) {
+	
+	public Member(int id, String name, String email, String password) {
 		this.id = id;
 		this.name = name;
+		this.setEmail(email);
+		this.password = password;
+	}
+	
+	@Column(name="password", length = 45)
+	@Length(max=45)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Column(name="email", length = 45)
+	@Length(max=254)
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
